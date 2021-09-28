872675e99d1406872e5f1d891b78f228957a99f9
Sairoden <odingandarosa@gmail.com>
2021-09-28 20:04:18 +0800

    Set Up Redux

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import reportWebVitals from "./reportWebVitals";
 
 // Redux Setup
 import { createStore } from "redux";
+import rootReducer from "./reducers";
+import { Provider } from "react-redux";
 
-const store = createStore();
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -1,8 +1,13 @@
 const initState = {
   popular: [],
-  
+  newGames: [],
+  upcoming: [],
+  searched: [],
 };
 
-const gamesReducer = (state, action) => {};
+const gamesReducer = (state = initState, action) => {
+  if (action.type === "FETCH_GAME") return { ...state };
+  return { ...state };
+};
 
 export default gamesReducer;
