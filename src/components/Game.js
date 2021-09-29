31a8f84196dfaf0911ba9a7a3ce71d5dc716248e
Sairoden <odingandarosa@gmail.com>
2021-09-29 19:35:22 +0800

    style cards

---
+++ src/components/Game.js
@@ -0,0 +1,28 @@
+// Styling and Animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const Game = ({ game }) => {
+  return (
+    <StyledGame>
+      <h3>{game.name}</h3>
+      <p>{game.released}</p>
+      <img src={game.background_image} alt={game.name} />
+    </StyledGame>
+  );
+};
+
+const StyledGame = styled(motion.div)`
+  min-height: 30vh;
+  box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
+  text-align: center;
+  border-radius: 1rem;
+
+  img {
+    width: 100%;
+    height: 40vh;
+    object-fit: cover;
+  }
+`;
+
+export default Game;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,52 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 
 import { loadGames } from "../actions/gamesAction";
+
+// Components
+import Game from "../components/Game";
+
+// Styling and Animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Home = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadGames());
-  });
+  }, [dispatch]);
+
+  // Get that data back
+  const { popular, upcoming, newGames } = useSelector(state => state.games);
+  console.log(upcoming);
 
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <GameList>
+      <h1>JERALDEN JOLORAN</h1>
+      <h2>Upcoming Games</h2>
+      <Games>
+        {upcoming.map(game => (
+          <Game game={game} key={game.id}></Game>
+        ))}
+      </Games>
+    </GameList>
   );
 };
+
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
 
 export default Home;
