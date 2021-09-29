65a9a6da33a5c54029e3a346d533e19f308c0d8f
Sairoden <odingandarosa@gmail.com>
2021-09-29 11:46:17 +0800

    Remove config env

--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -7,7 +7,14 @@ const initState = {
 
 const gamesReducer = (state = initState, action) => {
   if (action.type === "FETCH_GAME") return { ...state };
-  return { ...state };
+  return { ...state, ...action.payload };
+};
+
+// ACTION CREATOR
+const fetchGames = userData => {
+  return {
+    type: "FETCH_GAMES",
+  };
 };
 
 export default gamesReducer;
