7c7476d97c0eb025441712277663ed79276fc23a
Sairoden <odingandarosa@gmail.com>
2021-09-30 17:11:34 +0800

    Style Game Detail

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -12,26 +12,26 @@ const GameDetail = () => {
   return (
     <CardShadow>
       <Detail>
-        <div className="stats">
+        <Stats>
           <div className="rating">
             <h3>{game.name}</h3>
             <p>Rating: {game.rating}</p>
           </div>
-          <div className="info">
+          <Info>
             <h3>Platforms</h3>
-            <div className="platforms">
+            <Platforms>
               {game.platforms.map(platform => (
                 <h3 key={platform.platform.id}>{platform.platform.name}</h3>
               ))}
-            </div>
-          </div>
-        </div>
-        <div className="media">
+            </Platforms>
+          </Info>
+        </Stats>
+        <Media>
           <img src={game.background_image} alt={game.name} />
-        </div>
-        <div className="description">
+        </Media>
+        <Description>
           <p>{game.description_raw}</p>
-        </div>
+        </Description>
         <div className="gallery">
           {screenshot.results.map(screen => (
             <img key={screen.id} src={screen.image} alt={game.name}></img>
@@ -69,9 +69,44 @@ const Detail = styled(motion.div)`
   border-radius: 1rem;
   color: #333;
   left: 10%;
-  padding: 2rem 20rem;
+  padding: 2rem 5rem;
   position: absolute;
   width: 80%;
+
+  img {
+    width: 100%;
+  }
+`;
+
+const Stats = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Info = styled(motion.div)`
+  text-align: center;
+`;
+
+const Platforms = styled(motion.div)`
+  display: flex;
+  justify-content: space-evenly;
+
+  img {
+    margin-left: 3rem;
+  }
+`;
+
+const Media = styled(motion.div)`
+  margin-top: 5rem;
+
+  img {
+    width: 100%;
+  }
+`;
+
+const Description = styled(motion.div)`
+  margin: 5rem 0rem;
 `;
 
 export default GameDetail;
